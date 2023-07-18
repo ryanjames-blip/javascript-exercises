550165e72b44de0cbae8697ea1c229c576b45aa0
Ryan <ryandenbleyker@gmail.com>
2023-07-18 12:27:11 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+        [firstNum, secondNum] = [secondNum, firstNum];
+    }
+    if (typeof firstNum != 'number' ||
+        typeof secondNum != 'number' ||
+        firstNum < 0 || secondNum < 0) {
+            return 'ERROR';
+        }
 
+    let sum = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
