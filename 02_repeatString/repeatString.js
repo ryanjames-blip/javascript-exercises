752d0d67707db64bd555a91a108da2b834167306
Ryan <ryandenbleyker@gmail.com>
2023-07-18 10:53:13 -0400

    complete repeatstring exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    const originalString = string;
+    if (num == 0) {
+        return '';
+    }
+    else if (num == 1) {
+        return string;
+    }
+    else if (num > 1) {
+        for (let i = 0; i < num - 1; i++) {
+            string += originalString;
+        }
+        return string;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
