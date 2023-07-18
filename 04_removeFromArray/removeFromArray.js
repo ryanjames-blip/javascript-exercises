71e3e66cc7f74702aa26c5be4b58b2844d8320c9
Ryan <ryandenbleyker@gmail.com>
2023-07-18 12:10:55 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementToRemove) {
+    //iterate through the given array
+    for (let i = 0; i < array.length; i++) {
+        //iterate through the element(s)toRemove
+        for (let j = 0; j < elementToRemove.length; j++) {
+            if (array[i] === elementToRemove[j]) {
+                array.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
