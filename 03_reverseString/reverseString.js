c22d317bf4001771711184fdaa00720086e272d4
Ryan <ryandenbleyker@gmail.com>
2023-07-18 11:11:04 -0400

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reversed = stringArray.reverse();
+    return reversed.join('');
 };
 
 // Do not edit below this line
