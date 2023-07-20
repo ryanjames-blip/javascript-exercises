951ba859087b3e750b75c4ba1c5f17b028a5169c
Ryan <ryandenbleyker@gmail.com>
2023-07-20 14:32:10 -0400

    Complete findTheOldest.js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = {
+            name: 'Blank',
+            age: 0,
+        };
+    let personAge;
+    
+    for (let i = 0; i < people.length; i++) {
+        if (!('yearOfDeath' in people[i])) {
+            people[i].yearOfDeath = 2023;
+        }
+        personAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (personAge > oldest.age) {
+            oldest.age = personAge;
+            oldest.name = people[i].name;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
