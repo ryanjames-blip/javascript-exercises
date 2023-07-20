88aa9ea5193739e630457a3e2a9acfb93b2af341
Ryan <ryandenbleyker@gmail.com>
2023-07-20 18:44:32 -0400

    TODO COMPLETE THIS EXERCISE

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let stringArray = str.split('');
+    let compareArray = [...stringArray];
+    compareArray.reverse();
+    for (let i = 0; i < stringArray.length; i++) {
+        if (compareArray[i] != stringArray[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
