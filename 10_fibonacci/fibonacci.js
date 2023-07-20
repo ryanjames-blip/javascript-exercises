caea820fcfcef266f1eb0ddd206e24b91b546409
Ryan <ryandenbleyker@gmail.com>
2023-07-20 13:57:50 -0400

    Complete fibonacci.js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1,1];
+    let temp = fib[0];
 
+    if (typeof num != 'number') {
+        num = parseInt(num);
+    }
+
+    if (num < 1) return 'OOPS';
+
+    for (let i = 1; i < num - 1; i++) {
+        temp = fib[i-1] + fib[i];
+        fib.push(temp);
+    }
+    return temp;
 };
 
 // Do not edit below this line
